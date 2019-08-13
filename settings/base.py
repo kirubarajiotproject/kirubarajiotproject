3121e863857f5ef8f874ae99ea584c3ee00d7fa7
kirubarajiotproject <54075409+kirubarajiotproject@users.noreply.github.com>
2019-08-13 22:25:10 +0530

    Add files via upload

---
+++ settings/base.py
@@ -0,0 +1,9 @@
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
